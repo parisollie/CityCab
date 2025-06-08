1131373592dc52237182f5285e179b44023e58f4
parisollie <pjff18@gmail.com>
2025-06-08 16:38:31 -0600

    Initial Commit

---
+++ CityCab/CityCabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityCabApp.swift
+//  CityCab
+//
+//  Created by Paul F on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CityCabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
